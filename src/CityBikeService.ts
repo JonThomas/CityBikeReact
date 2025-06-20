b5262f4a01dd1efd30de4afa74d5fbe82c70853f
Jon-Thomas Eliassen <jonel@samfundet.no>
2025-06-20 19:27:11 +0200

    Added optional client identification header

--- src/CityBikeService.ts
+++ src/CityBikeService.ts
@@ -1,11 +1,15 @@
 import type { Station } from './types/Station';
 import type { StationStatus } from './types/StationStatus';
 
+const header = {
+  'Client-Identifier': 'JonThomasEliassen-CityBikeReact',
+};
+
 export const fetchMergedData = async () => {
   const [rawStations, rawStationStatus] = await Promise.all([
-    fetch('https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json')
+    fetch('https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json' , { headers: header }  )
       .then(res => res.json()),
-    fetch('https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json')
+    fetch('https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json' , { headers: header }  )
       .then(res => res.json())
   ]);
 
