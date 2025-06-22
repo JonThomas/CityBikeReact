8a7fa06ba74e2a96afd4f7b19ab40d01bc1e7ec2
Jon-Thomas Eliassen <jonel@samfundet.no>
2025-06-22 21:42:10 +0200

    Removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import StationFetcher from './components/StationFetcher/StationFetcher'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>         {/* React Fragment */}
       <div>
